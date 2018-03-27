ea6f3abf70c0f34305d200ad9321f592e725d5a6
Philippe Niles <philippeniles@Philippes-MacBook-Pro.local>
2018-03-26 22:53:38 -0400

    add population start/ change BackendTables

--- src/Population.sql
+++ src/Population.sql
@@ -1,1 +1,6 @@
 -- We add values to the RESTAURANT table.
+INSERT INTO RATER ('1','superSM@gmail.com','SuperSizeMe',DEFAULT,'Documentary',4);
+INSERT INTO RATER ('2','tHistoryoFood@gmail.com','The_History_of_Food',DEFAULT,'Blog',4);
+INSERT INTO RATER ('3','food52@gmail.com','Food_52',DEFAULT,'Blog',3);
+INSERT INTO RATER ('4','fedUp@gmail.com','Fed_Up',DEFAULT,'Documentary',4);
+INSERT INTO RATER ('5','PinchOfYum@gmail.com','Pinch_of_Yum',DEFAULT,'Blog',3);
