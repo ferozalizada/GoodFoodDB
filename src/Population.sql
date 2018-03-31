3f35dfb8dfe444db330398d1261b9e9e27d0cb44
Philippe Niles <philippeniles@Philippes-MacBook-Pro.local>
2018-03-31 14:35:55 -0400

    update database Population

--- src/Population.sql
+++ src/Population.sql
@@ -4,3 +4,40 @@ INSERT INTO RATER ('2','tHistoryoFood@gmail.com','The_History_of_Food',DEFAULT,'
 INSERT INTO RATER ('3','food52@gmail.com','Food_52',DEFAULT,'Blog',3);
 INSERT INTO RATER ('4','fedUp@gmail.com','Fed_Up',DEFAULT,'Documentary',4);
 INSERT INTO RATER ('5','PinchOfYum@gmail.com','Pinch_of_Yum',DEFAULT,'Blog',3);
+INSERT INTO RATER ('6','A.Mahn@gmail.com','Ali_Mahn',DEFAULT,'Blog',2);
+INSERT INTO RATER ('7','pnile029@uottawa.ca','Philippe_Niles',DEFAULT,'Food Critic',5);
+INSERT INTO RATER ('8','M.desmond@gmail.com','Michael_Desmond',DEFAULT,'Online',3);
+INSERT INTO RATER ('9','O.Jennings@gmail.com','Olivia_Jennings',DEFAULT,'Food Critic',5);
+INSERT INTO RATER ('10','I.Yoland@gmail.com','Ilanda_Yoland',DEFAULT,'Blog',1);
+INSERT INTO RATER ('11','M.Kennedy@gmail.com','Maxim_Kennedy',DEFAULT,'Online',3);
+INSERT INTO RATER ('12','L.Marin@gmail.com','Lise_Marin',DEFAULT,'Blog',2);
+INSERT INTO RATER ('13','D.Henry@gmail.com','Danielle_Henry',DEFAULT,'Food Critic',5);
+INSERT INTO RATER ('14','ottawacooks@gmail.com','ottawa_cooks',DEFAULT,'Online',5);
+INSERT INTO RATER ('15','feroz@gmail.com','Feroz',DEFAULT,'Blog',5);
+
+
+INSERT INTO RESTAURANT ('1','Burgers and Fries Forever','Fast Food','https://burgersnfriesforever.com');
+INSERT INTO RESTAURANT ('2','Mellos','Breakfast','https://mellos.com');
+INSERT INTO RESTAURANT ('3','Sushi Hello','Asian','https://sushiHello.com');
+INSERT INTO RESTAURANT ('4','Freddies Bistro','Diner','https://Freddies.com');
+INSERT INTO RESTAURANT ('5','La Mangue Amere','African','https://lamangueamere.com');
+INSERT INTO RESTAURANT ('6','Mario\'s Pizza','Pizza','https://MarriosPizza.com');
+INSERT INTO RESTAURANT ('7','Luigi\'s Linguini','Italian','https://LuigisLinguini.com');
+INSERT INTO RESTAURANT ('8','Yangtze','Chinese','https://yangtze.com');
+INSERT INTO RESTAURANT ('9','Shawarma Prince','Fast Food','https://shawarmaprince.com');
+INSERT INTO RESTAURANT ('10','Coconut pond','Indian','https://coconutpond.com');
+INSERT INTO RESTAURANT ('11','Sweet God','Ice Cream','https://sweetgod.com');
+INSERT INTO RESTAURANT ('12','Hello World','Fast Food','https://helloworldrestaurant.com');
+
+INSERT INTO LOCATION ('1','18-01-2016','Habib El Khachab','(613) 241-3456.','278 Dalhousie Street','8h','22h','1');
+INSERT INTO LOCATION ('2','20-04-2000','John Malone','(613) 890-2234.','900 York Street','8h','17h','2');
+INSERT INTO LOCATION ('3','10-12-2010','Mariot Bigsby','(613) 338-8890.','7889 Catherine Street','8h','22h','3');
+INSERT INTO LOCATION ('4','02-08-1998','Freddie Marsh','(613) 499-4390.','2234 Rideau Street','8h','22h','4');
+INSERT INTO LOCATION ('5','30-02-2002','Ernesto Nistrali','(613) 555-2334.','221 Marier Street','8h','22h','5');
+INSERT INTO LOCATION ('6','12-09-1996','Mairo Hartby','(613) 112-1432.','222 Crichton Street','8h','22h','6');
+INSERT INTO LOCATION ('7','03-05-2018','Luigi Hartby','(613) 412-0678.','123 Beechwood Street','8h','22h','7');
+INSERT INTO LOCATION ('8','01-01-2017','Lee Ming','(613) 232-5678.','980 Montreal Road','8h','22h','8');
+INSERT INTO LOCATION ('9','16-01-2018','Patrick Taggart','(613) 550-1239.','230 Dalhousie Street','8h','22h','9');
+INSERT INTO LOCATION ('10','11-11-2012','Thomas Thottungal','(613) 334-7866.','1000 Slater Street','8h','22h','10');
+INSERT INTO LOCATION ('11','22-06-2014','Bob Blunder','(613) 223-3236.','900 King Edward Street','11h','22h','11');
+INSERT INTO LOCATION ('12','24-03-2001','Ernie Java','(613) 233-2345.','200 Dalhousie Street','8h','22h','12');
