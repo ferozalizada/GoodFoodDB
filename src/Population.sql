86e407d04b86e275f2a16ff0bad6f8f9ae9e2247
Philippe Niles <philippeniles@Philippes-MacBook-Pro.local>
2018-03-31 21:08:22 -0400

    update population insertions

--- src/Population.sql
+++ src/Population.sql
@@ -29,7 +29,7 @@ INSERT INTO RESTAURANT ('10','Coconut pond','Indian','https://coconutpond.com');
 INSERT INTO RESTAURANT ('11','Sweet God','Ice Cream','https://sweetgod.com');
 INSERT INTO RESTAURANT ('12','Hello World','Fast Food','https://helloworldrestaurant.com');
 
-INSERT INTO LOCATION ('1','18-01-2016','Habib El Khachab','(613) 241-3456.','278 Dalhousie Street','8h','22h','1');
+INSERT INTO LOCATION ('1','18-01-2016','Ziad El Khachab','(613) 241-3456.','278 Dalhousie Street','8h','22h','1');
 INSERT INTO LOCATION ('2','20-04-2000','John Malone','(613) 890-2234.','900 York Street','8h','17h','2');
 INSERT INTO LOCATION ('3','10-12-2010','Mariot Bigsby','(613) 338-8890.','7889 Catherine Street','8h','22h','3');
 INSERT INTO LOCATION ('4','02-08-1998','Freddie Marsh','(613) 499-4390.','2234 Rideau Street','8h','22h','4');
@@ -41,3 +41,63 @@ INSERT INTO LOCATION ('9','16-01-2018','Patrick Taggart','(613) 550-1239.','230 
 INSERT INTO LOCATION ('10','11-11-2012','Thomas Thottungal','(613) 334-7866.','1000 Slater Street','8h','22h','10');
 INSERT INTO LOCATION ('11','22-06-2014','Bob Blunder','(613) 223-3236.','900 King Edward Street','11h','22h','11');
 INSERT INTO LOCATION ('12','24-03-2001','Ernie Java','(613) 233-2345.','200 Dalhousie Street','8h','22h','12');
+
+INSERT INTO MENUITEM (1,'Cheeseburger','Food','Main','cheese, letuce, tomato, canadian beef pattie',10.00,'1');
+INSERT INTO MENUITEM (2,'The works','Food','Main','Caramelised onions, tomato, letuce, sauteed mushrooms, avocado, bacon, canadian beef pattie',12.00,'1');
+INSERT INTO MENUITEM (3,'The Frenchman','Food','Main','Pear, brie, caramelised onions, canadian beef pattie',15.00,'1');
+INSERT INTO MENUITEM (4,'Fan Favorite','Food','Main','tomato, bacon, caramelised onions, sauteed mushrooms, bbq sauce, canadian beef pattie',15.00,'1');
+
+INSERT INTO MENUITEM (5,'Blueberry waffles','Food','Main','2 large blueberry waffles',10.00,'2');
+INSERT INTO MENUITEM (6,'The Classic','Food','Main','2 eggs, bacon, sausage, baked beans, toasts',9.50,'2');
+INSERT INTO MENUITEM (7,'Hungry Man','Food','Main','4 eggs, bacon, sausages, baked beans, one waffle, toasts',14.00,'2');
+INSERT INTO MENUITEM (8,'Vege Breaky','Food','Main','lentil oatmeal cooked in vegetable broth served with avocado, tomatoes, hummus, and salsa',11.00,'2');
+
+INSERT INTO MENUITEM (9,'California roll','Food','Main','Crab sitck, avocado, cucumber',7.00,'3');
+INSERT INTO MENUITEM (10,'Dynamite roll','Food','Main','Tempura shrimp, avocado',7.00,'3');
+INSERT INTO MENUITEM (11,'Sake','Drink','Drink','Hot sake (rice wine)',10.00,'3');
+INSERT INTO MENUITEM (12,'Kappa roll','Food','Main','cucumber',5.00,'3');
+
+INSERT INTO MENUITEM (13,'Chili dog','Food','Main','Homemade chili on a hotdog',12.00,'4');
+INSERT INTO MENUITEM (14,'Chili poutine','Food','Main','Homemade chili on a poutine',15.00,'4');
+INSERT INTO MENUITEM (15,'Chili','Food','Main','Homemade chili',12.00,'4');
+INSERT INTO MENUITEM (16,'Cheese burger','Food','Main','tomato, letuce, cheese, canadian beef',14.00,'4');
+
+INSERT INTO MENUITEM (17,'South african stew','Food','Main','Assortment of vegetables cooked in a peanut sauce',15.00,'5');
+INSERT INTO MENUITEM (18,'Jerk seasoned fish with steamed vegetables','Food','Main','Fish of the day seasoned with jerk rub',19.00,'5');
+INSERT INTO MENUITEM (19,'Fish of the day','Food','Main','Grilled fish of the say season with a bit of lemon and served with a green salad',20.00,'5');
+INSERT INTO MENUITEM (20,'Soup of the day','Food','Starter','',10.00,'5');
+
+INSERT INTO MENUITEM (21,'Cheese pizza','Food','Main','Mozzarella, tomato sauce',100.00,'6');
+INSERT INTO MENUITEM (22,'Canadian pizza','Food','Main','Mozarella, bacon, mushroom, peperoni',100.00,'6');
+INSERT INTO MENUITEM (23,'All dressed','Food','Main','Mozzarella, onions, green peppers, mushroom, peperoni',100.00,'6');
+INSERT INTO MENUITEM (24,'Vegetarian','Food','Main','Mozzarella, spinach, tomatoes, onions, red peppers, mushroom',100.00,'6');
+
+INSERT INTO MENUITEM (25,'Linguini alfredo','Food','Main','Long flat pasta in a white cream sauce',16.00,'7');
+INSERT INTO MENUITEM (26,'Spaghetti bolognese','Food','Main','Long thin pasta, in a tomato beef sause',20.00,'7');
+INSERT INTO MENUITEM (27,'Soup of the day','Food','Starter','Soup of the day',12.00,'7');
+INSERT INTO MENUITEM (28,'Caesar salad','Food','Starter','Romaine letuce with croutons, bacon, shredded parmesian, caeser dressing',11.00,'7');
+
+INSERT INTO MENUITEM (29,'Pork bun','Food','Starter','Bbq pork in a steamed bun',10.00,'8');
+INSERT INTO MENUITEM (30,'Shreaded sichuan chicken','Food','Main','Shreaded chicken with vegetables cooked in sechuan sauce',16.00,'8');
+INSERT INTO MENUITEM (30,'White rice','Food','Side','White sticky rice',6.00,'8');
+INSERT INTO MENUITEM (31,'Spring rolls','Food','Starter','4 vegetable spring rolls',7.00,'8');
+
+INSERT INTO MENUITEM (32,'Shawarma plater','Food','Main','Halal chicken, garlic potatoes, seasoned beets, greek salad',300.00,'9');
+INSERT INTO MENUITEM (33,'Shawarma sandwhich','Food','CATEGORY','Your choice of meat, seasoned beets, tabouleh, galric sauce, salsa',300.00,'9');
+INSERT INTO MENUITEM (34,'Falafel','Food','CATEGORY','',300.00,'9');
+INSERT INTO MENUITEM (35,'Greek salad','Food','CATEGORY','',300.00,'9');
+
+INSERT INTO MENUITEM (36,'Butter chicken','Food','Main','A fan favorite',22.00,'10');
+INSERT INTO MENUITEM (37,'Egg Mappas','Food','Main','Boiled Eggs in Curried Onion Sauce spiked with crushed Pepper',21.00,'10');
+INSERT INTO MENUITEM (38,'Paneer Spinach Kurma','Food','Main','Cottage Cheese cooked in a rich Spinach Sauce',21.00,'10');
+INSERT INTO MENUITEM (39,'Naddan Vegetable Kurma','Food','Main','Assorted Seasonal Vegetable cooked in Coconut gravy',21.00,'10');
+
+INSERT INTO MENUITEM (40,'CHOCOLATE, PEANUT BUTTER, CARAMEL','Food','Desert','Vanilla soft serve, peanut butter sauce, caramel sauce, chocolate peanut crumble, chocolate sauce',10.00,'11');
+INSERT INTO MENUITEM (41,'KRUSTY THE CONE','Food','Desert','Vanilla soft serve, cotton candy, cotton candy sauce, rainbow sprinkles',10.00,'11');
+INSERT INTO MENUITEM (42,'ROCKY ROAD RAGE','Food','Desert','Chocolate soft serve, marshmallows, chocolate sauce, walnuts, cashew & Oreo cookie crumble',10.00,'11');
+INSERT INTO MENUITEM (43,'BANGIN\' BROWNIE','Food','Deset','Chocolate soft serve, caramel, chocolate sauce, brownie bits, chocolate cookie crumble',10.00,'11');
+
+INSERT INTO MENUITEM (44,'Java','Food','Main','Object oriented delight',10.00,'12');
+INSERT INTO MENUITEM (45,'Python','Food','Main','Scripts for days',300.00,'12');
+INSERT INTO MENUITEM (46,'Go','Food','Main','Multithread fanfare',5.00,'12');
+INSERT INTO MENUITEM (47,'C','Food','Main','The grandpa',1.00,'12');
